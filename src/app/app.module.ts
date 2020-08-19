000844f891f15c53cc56e531c4174f2aed947b4a
David <39265971+somervda@users.noreply.github.com>
2020-08-19 14:20:36 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,111 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirePerformanceModule } from "@angular/fire/performance";
+
+import { environment } from "../environments/environment";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+// import { AngularFireStorageModule } from "@angular/fire/storage";
+import { AngularFireStorageModule } from "@angular/fire/storage";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatChipsModule } from "@angular/material/chips";
+import {
+  MatDividerModule,
+  // MatDatepickerModule,
+  // MatDialogModule,
+  // MatInputModule,
+  // MatSelectModule,
+  // MatGridListModule
+} from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+
+import { NgbCarouselModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { HomeComponent } from "./home/home.component";
+import { NotfoundComponent } from "./notfound/notfound.component";
+import { AboutComponent } from "./about/about.component";
+import { LoginComponent } from "./login/login.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { UsersComponent } from "./users/users.component";
+import { UserComponent } from "./user/user.component";
+import { NotauthorizedComponent } from "./notauthorized/notauthorized.component";
+import { DocPipe } from "./pipes/doc.pipe";
+import { SubheadingComponent } from "./shared/subheading/subheading.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NotfoundComponent,
+    AboutComponent,
+    LoginComponent,
+    UsersComponent,
+    UserComponent,
+    NotauthorizedComponent,
+    DocPipe,
+    SubheadingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AppRoutingModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatDividerModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    // MatMenuModule,
+    // MatDividerModule,
+    // MatTabsModule,
+    // MatInputModule,
+    // MatTableModule,
+    // MatDialogModule,
+    // MatSelectModule,
+    // MatDatepickerModule,
+    // MatMomentDateModule,
+    AppRoutingModule,
+    NgbCarouselModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirePerformanceModule,
+    AngularFireAuthModule,
+    // Allow offline operations - useful when used in combination with PWA functionality
+    // AngularFirestoreModule,
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireStorageModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production,
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from "@angular/core";
+import { AngularFireStorage } from "@angular/fire/storage";
+import { ActivatedRoute } from "@angular/router";
+import { Observable } from "rxjs";
+import { User } from "../models/user.model";
+import { HelperService } from "../services/helper.service";
+import { AuthService } from "./../services/auth.service";
+import { UserService } from "./../services/user.service";
+
+@Component({
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"],
+})
+export class UserComponent implements OnInit {
+  user$: Observable<User>;
+  uid: string;
+  // If only the photo can be updated unless the user has fullAccess
+  fullAccess: boolean = false;
+
+  showSpinner = false;
+  fileUploadMsg = "";
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private auth: AuthService,
+    private storage: AngularFireStorage,
+    private helper: HelperService
+  ) {}
+
+  ngOnInit() {
+    this.uid = this.route.snapshot.paramMap.get("uid");
+    if (
+      this.auth.currentUser.isAdmin &&
+      this.auth.currentUser.uid != this.uid
+    ) {
+      this.fullAccess = true;
+    }
+    if (
+      !this.auth.currentUser.isAdmin &&
+      this.auth.currentUser.uid != this.uid
+    ) {
+      this.helper.redirect("/notAuthorized");
+    }
+
+    this.user$ = this.userService.findUserByUid(this.uid);
+  }
+
+  updateField(name: string, value: any) {
+    console.log("updateField", name, value);
+    if (this.fullAccess) {
+      this.userService.dbFieldUpdate(this.uid, name, value);
+    }
+  }
+
+  getStorageUrl(filename: string): Observable<any> {
+    // console.log("getStorageUrl", filename);
+    return this.storage
+      .ref(`userphotos/${this.uid}/${filename}`)
+      .getDownloadURL();
+  }
+
+  onUploadFile(event) {
+    console.log("onUploadFile", event);
+    const fileToUpload = event.target.files[0];
+    console.log("fileToUpload", fileToUpload.size);
+    if (fileToUpload.size > 150000) {
+      this.fileUploadMsg =
+        " File is too large to upload. Must be less than 150KB";
+    } else {
+      this.showSpinner = true;
+      this.fileUploadMsg = "";
+      const task = this.storage
+        .upload(`userphotos/${this.uid}/${fileToUpload.name}`, fileToUpload)
+        .then((t) => {
+          this.getStorageUrl(fileToUpload.name)
+            .toPromise()
+            .then((url) =>
+              this.userService.dbFieldUpdate(this.uid, "photoURL", url)
+            );
+          this.showSpinner = false;
+        })
+        .catch((e) => (this.showSpinner = false));
+    }
+  }
+
+  getDate(inDate: any): string {
+    let sDate = "";
+    if (inDate && inDate != null) {
+      sDate = inDate.toDate();
+    }
+
+    return sDate;
+  }
+}
