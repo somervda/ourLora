13a81cc03a0b632c421932ddd28827269a7cf2a7
David <39265971+somervda@users.noreply.github.com>
2020-09-01 08:33:07 -0400

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,10 @@ import * as functions from "firebase-functions";
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const mailbox = functions.https.onRequest((request, response) => {
+  // Test with
+  //  curl -d 'name=david&age=12&payload_fields={"lat": 40.1746711730957,"lng": -75.30223083496094}' https://us-central1-ourlora-6afb9.cloudfunctions.net/mailbox
   console.log("request.body.payload_fields:", request.body.payload_fields);
+  console.log("request.body.name:", request.body.name);
   //   console.log("request.header:", request.header);
   response.status(200).send("Hello from Firebase!");
 });
