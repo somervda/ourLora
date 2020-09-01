a625cfb7e37124cd29b027e11ab6f54c7f15e9a9
David <39265971+somervda@users.noreply.github.com>
2020-09-01 08:12:12 -0400

    added a function to recieve mailbox messages I had to update the functios typescript version and yje node level to 10, also I had to change to the blaze plan when using node 10

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,14 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
 // import * as admin from "firebase-admin";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const mailbox = functions.https.onRequest((request, response) => {
+  console.log("request.body.payload_fields:", request.body.payload_fields);
+  //   console.log("request.header:", request.header);
+  response.status(200).send("Hello from Firebase!");
+});
 
 // exports.usersDateCreated = functions.firestore
 //   .document("users/{uid}")
