5f46fce04ccc1c2c5a21608acab26ba9c508ad6b
David  on Go <davidsomerville@comcast.net>
2020-09-16 11:14:03 -0400

    wip - in progress on sensor

--- src/app/models/sensor.model.ts
+++ src/app/models/sensor.model.ts
@@ -16,9 +16,53 @@ export enum UOM {
   "volts" = 2,
   "degrees" = 3,
   "bit" = 4,
-  "relativeHumidity" = 5,
+  "RH" = 5,
   "pascal" = 6,
-  "meters/sec" = 7,
+  "meters_sec" = 7,
   "meters" = 8,
   "dimensionless" = 9,
 }
+
+export interface UOMInfoItem {
+  uom: UOM;
+  name: string;
+}
+
+export const UOMInfo: UOMInfoItem[] = [
+  {
+    uom: UOM.bit,
+    name: "Bit (On/Off, 0/1)",
+  },
+  {
+    uom: UOM.centigrade,
+    name: "Centigrade (Temperature)",
+  },
+  {
+    uom: UOM.degrees,
+    name: "Degrees (Angle 0-360)",
+  },
+  {
+    uom: UOM.dimensionless,
+    name: "Dimensionless (Pure scaler)",
+  },
+  {
+    uom: UOM.meters,
+    name: "Meters (Length)",
+  },
+  {
+    uom: UOM.meters_sec,
+    name: "M/s (speed)",
+  },
+  {
+    uom: UOM.pascal,
+    name: "Pascal (Pressure)",
+  },
+  {
+    uom: UOM.RH,
+    name: "RH (Relative Humidity%)",
+  },
+  {
+    uom: UOM.volts,
+    name: "Volts",
+  },
+];
