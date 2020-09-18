d08c9b942c74539cc0f88464abe81048238f2178
David <39265971+somervda@users.noreply.github.com>
2020-09-18 17:12:54 -0400

    Cleaning up aquisition function

--- functions/src/acquisition.ts
+++ functions/src/acquisition.ts
@@ -1,14 +1,17 @@
 import * as functions from "firebase-functions";
-// import * as admin from "firebase-admin";
 
 export const mailbox = functions.https.onRequest((request, response) => {
-  // Test with curl
-  //  curl -d '{"payload_fields" : {"lat": 40.1746711730957,"lng": -75.30223083496094, "dev_id": "curlTest01"}}' -H 'Content-Type: application/json' --user-agent "curl" --user ourLora:password https://ourLora.com/mailbox
-  if (request.headers.authorization == "Basic b3VyTG9yYTpwYXNzd29yZA==") {
+  // To test with curl
+  //  curl -d '{"payload_fields" : {"lat": 40.1746711730957,"lng": -75.30223083496094, "dev_id": "curlTest01", "TEMPERATURE": 99}}' -H 'Content-Type: application/json' --user-agent "curl" --user ourLora:password https://ourLora.com/mailbox
+
+  const isValidAuthorization =
+    request.headers.authorization == "Basic b3VyTG9yYTpwYXNzd29yZA==";
+  const isValidContentType =
+    request.headers["content-type"] == "application/json";
+
+  if (isValidAuthorization && isValidContentType) {
     // Basic authorization = looking for ourLora:password  as the authorization info
-    // console.log("request.body.payload_fields:", request.body.payload_fields);
-    // console.log("request.headers", request.headers);
-    // console.log("request.body:", request.body);
+    // Body content tpe must be json
     const userAgent = request.headers["user-agent"]?.split("/", 2);
     if (userAgent) {
       switch (userAgent[0]) {
@@ -16,8 +19,9 @@ export const mailbox = functions.https.onRequest((request, response) => {
           const deviceId = request.body.payload_fields.deviceId;
           // SIG fox, you must add the "deviceId : {device}"  property to the to the payload_fields json
           console.log("From SIGFOX , deviceId:", deviceId);
+          writeSensorEvent(deviceId, request.body);
           response.status(200).send("Hello SIGFOX ");
-          break;
+          return;
         }
         case "http-ttn": {
           //statements;
@@ -25,39 +29,46 @@ export const mailbox = functions.https.onRequest((request, response) => {
           // TTN always provides the device id as part as the body json
           const deviceId = request.body.dev_id;
           console.log("From TTN , deviceId:", deviceId);
+          writeSensorEvent(deviceId, request.body);
           response.status(200).send("Hello TTN!");
-          break;
+          return;
         }
         case "curl": {
           //statements;
           const deviceId = request.body.payload_fields["dev_id"];
-          console.log(
-            "From curl , deviceId:",
-            deviceId,
-            request.body.payload_fields
-          );
-          writeSensorEvent(deviceId, request.body.payload_fields);
+          console.log("From curl , deviceId:", deviceId);
+          writeSensorEvent(deviceId, request.body);
           response.status(200).send("Hello curl - " + deviceId);
-          break;
+          return;
         }
         default: {
           //statements;
           console.log("Invalid user agent:", request.headers["user-agent"]);
           response.status(412).send("Precondition Failed");
-          break;
+          return;
         }
       }
     } else {
       console.log("Missing user agent:");
       response.status(412).send("Precondition Failed");
+      return;
     }
-  } else {
+  }
+
+  if (!isValidAuthorization) {
     console.log("401 Unauthorized:", request.headers.authorization);
     response.status(401).send("Unauthorized");
+    return;
+  }
+
+  if (!isValidContentType) {
+    console.log("Invalid content type: ", request.headers["content-type"]);
+    response.status(412).send("Precondition Failed");
+    return;
   }
 });
 
-function writeSensorEvent(deviceId: string, payload: object): number {
-  console.log("writeSensorEvent:", deviceId, payload);
-  return 1;
+function writeSensorEvent(deviceId: string, requestBody: object) {
+  console.log("writeSensorEvent:", deviceId, JSON.stringify(requestBody));
+  return;
 }
