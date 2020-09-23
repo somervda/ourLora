74b9a75e7e95a00cc12fddff41d2cfb4dffb071b
David  on Go <davidsomerville@comcast.net>
2020-09-23 12:52:05 -0400

    removed sensor timestamp property and changed acqisition fields in the sensor component to us media query to determine how many columns to show

--- functions/src/models/sensor.model.ts
+++ functions/src/models/sensor.model.ts
@@ -3,7 +3,6 @@ export interface Sensor {
   name: string;
   description: string;
   uom: UOM;
-  acquisitionMapTimestamp: string;
   acquisitionMapValue: string;
   acquisitionMapLatitude: string;
   acquisitionMapLongitude: string;
